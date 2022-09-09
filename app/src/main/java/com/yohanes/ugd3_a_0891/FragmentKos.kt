697078aeac34b09836d18b17cf68e4232eb448f9
yoyoh13 <yoyohdesti@gmail.com>
2022-09-09 21:42:37 +0700

    merge

--- app/src/main/java/com/yohanes/ugd3_a_0891/FragmentKos.kt
+++ app/src/main/java/com/yohanes/ugd3_a_0891/FragmentKos.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.transition.MaterialFadeThrough
+import com.yohanes.ugd3_a_0891.entity.Kos
 
 class FragmentKos : Fragment(R.layout.fragment_kos) {
 
