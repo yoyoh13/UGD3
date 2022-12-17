8a11a816156ce5e90adc7f2395cdd6d0c9dbbbb7
Prism00 <ryanvijaya8@gmail.com>
2022-12-17 21:37:46 +0700

    Camera di fragment fav

--- app/src/main/java/com/yohanes/ugd3_a_0891/fragment/FragmentFavorite.kt
+++ app/src/main/java/com/yohanes/ugd3_a_0891/fragment/FragmentFavorite.kt
@@ -1,18 +1,46 @@
 package com.yohanes.ugd3_a_0891.fragment
 
+import android.annotation.SuppressLint
+import android.hardware.Camera
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.google.android.material.transition.MaterialFadeThrough
+import com.yohanes.ugd3_a_0891.CameraView
 import com.yohanes.ugd3_a_0891.R
 
 
 class FragmentFavorite : Fragment(R.layout.fragment_favorite) {
 
+    private var mCamera: Camera? = null
+    private var mCameraView: CameraView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
 
-        exitTransition = MaterialFadeThrough()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        try{
+            mCamera = Camera.open()
+        }catch (e: Exception){
+            Log.d("Error", "Failed to get Camera" + e.message)
+        }
+        if(mCamera != null){
+            mCameraView = CameraView(this.context, mCamera!!)
+            val camera_view = view.findViewById<View>(R.id.FLCamera) as FrameLayout
+            camera_view.addView(mCameraView)
+        }
+        @SuppressLint("MissingInflatedId", "LocalSuppress") val imageClose =
+            view.findViewById<View>(R.id.imgClose) as ImageButton
+        imageClose.setOnClickListener{ view: View? -> System.exit(0)}
     }
 
 }
