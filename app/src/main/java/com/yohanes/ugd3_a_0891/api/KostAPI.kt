bfa8848ef84bd740f31a6b7b06f9cd4ff118964d
ryynld <henareynald@gmail.com>
2022-12-19 02:39:23 +0700

    fix

--- app/src/main/java/com/yohanes/ugd3_a_0891/api/KostAPI.kt
+++ app/src/main/java/com/yohanes/ugd3_a_0891/api/KostAPI.kt
@@ -2,7 +2,7 @@ package com.yohanes.ugd3_a_0891.api
 
 class KostAPI {
     companion object{
-        val BASE_URL = "http://192.168.100.6/Tubes_1_Kost/kost"
+        val BASE_URL = "http://127.0.0.1:8000/api/kost"
 
         val GET_ALL_URL = BASE_URL + "kost"
         val GET_BY_ID_URL = BASE_URL + "kost/"
--- app/src/main/java/com/yohanes/ugd3_a_0891/api/UserAPI.kt
+++ app/src/main/java/com/yohanes/ugd3_a_0891/api/UserAPI.kt
@@ -2,7 +2,7 @@ package com.yohanes.ugd3_a_0891.api
 
 class UserAPI {
     companion object{
-        val BASE_URL = "http://192.168.100.6/Tubes_1_Kost/user"
+        val BASE_URL = "http://127.0.0.1:8000/api/user"
 
         val GET_BY_ID_URL = BASE_URL + "user/"
         val ADD_URL = BASE_URL + "user"
